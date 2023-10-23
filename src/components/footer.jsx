2e930e735aa742d2442dcd186fd86b7a33a75a87
Cam <camila.dambrosio95@gmail.com>
2023-10-23 14:10:50 -0300

    added some styles

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,20 @@
 import './footer.css'
+import Facebook from '../assets/facebook.png'
+import Instagram from '../assets/instagram.png'
+import Twitter from '../assets/twitter.png'
 
 const Footer = () => {
 return (
     <footer className="footer">
         <ul className="navbar__items">
-          
-            <li className="navbar__item">
-                <a className="navbar__link" href="https://www.instagram.com/"> <img className="social" src="./img/instagram.png" alt="" /></a>
+            <li className="navbar__item smediaicon">
+                <a className="navbar__link" href="https://www.instagram.com/"> <img className="social" src={Instagram} alt="" /></a>
             </li>
-            <li className="navbar__item">
-                <a className="navbar__link" href="https://www.facebook.com/"> <img className="social" src="./img/facebook.png" alt="" /> </a>
+            <li className="navbar__item smediaicon">
+                <a className="navbar__link" href="https://www.facebook.com/"> <img className="social" src={Facebook} alt="" /> </a>
             </li>
-            <li className="navbar__item">
-                <a className="navbar__link" href="https://twitter.com/home"><img src="./img/x.png" className="social" alt="" /> </a>
+            <li className="navbar__item smediaicon">
+                <a className="navbar__link" href="https://twitter.com/home"><img src={Twitter} className="social" alt="" /> </a>
             </li>
         </ul>
 
