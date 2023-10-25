40b6a3b72634d9ee3259c79ebed449bafde94142
Cam <camila.dambrosio95@gmail.com>
2023-10-25 14:38:51 -0300

    installed axios and changed some stuff in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,44 @@
-import { Card, Strong, Inset, Text } from '@radix-ui/themes';
+import { Card, Inset, Text } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import './Card.css'
 
 
-const CardComponent =() => {
+// eslint-disable-next-line react/prop-types
+const CardComponent = ({evento}) => {
   return (
-  <div className='container'>
-    <Card size="2" style={{ maxWidth: 240 }}>
-      <Inset clip="padding-box" side="top" pb="current">
-        <img
-          src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
-          alt="Bold typography"
-          style={{
-            display: 'block',
-            objectFit: 'cover',
-            width: '100%',
-            height: 140,
-            backgroundColor: 'var(--gray-5)',
-          }}
-        />
-      </Inset>
-      <Text as="p" size="3">
-        <Strong>Typography</Strong> is the art and technique of arranging type to
-        make written language legible, readable and appealing when displayed.
-      </Text>
-    </Card>
-  </div>
+    <>
+      { evento != {} ? 
+        <div className='container'>
+
+          <Card size="2" style={{ maxWidth: 240 }}>
+            <Inset clip="padding-box" side="top" pb="current">
+              <img
+                src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
+                alt="Bold typography"
+                style={{
+                  display: 'block',
+                  objectFit: 'cover',
+                  width: '100%',
+                  height: 140,
+                  backgroundColor: 'var(--gray-5)',
+                }}
+              />
+            </Inset>
+            <Text as="p" size="3">
+              {/* eslint-disable-next-line react/prop-types */}
+              <h3>{evento.name ? evento.name : "no hay titulo"}</h3>
+              {/* eslint-disable-next-line react/prop-types */}
+              <p>{evento.description ? evento.description : "no hay descripción"}</p>
+              <button>details</button>
+            </Text>
+          </Card> 
+
+        </div>  
+
+        : <></>
+      } 
+
+    </>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,38 @@
 import './Main.css'
 import CardComponent from './Card.jsx'
-import SearchFilter from './Search-filter.jsx'
+import SearchFilter from './SearchFilter.jsx'
 import Checkbox from './Checkbox'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Main = () => {
+
+    let [eventos, setEventos] = useState([])
+
+    useEffect(() => {
+        axios.get("https://mindhub-xj03.onrender.com/api/amazing")
+        .then((response) => {
+            setEventos(response.data.events)
+        })
+    }, [])
+
+
+
     return (
         <>
-        <div className="search">
-            <Checkbox/>
-            <SearchFilter/>
-        </div>
-        <CardComponent />
+            <div className="search">
+                <Checkbox/>
+                <SearchFilter/>
+            </div>
+            <div className='event_cards'>
+            {
+                eventos.map((evento) => {
+                    return ( 
+                        // eslint-disable-next-line react/jsx-key
+                        <CardComponent evento={evento}/>
+                )})
+            }
+            </div>
         </>
     )
 }
