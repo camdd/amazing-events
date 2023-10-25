b6ba95474060ed7add3d1fe6e42b3d98e6508975
Cam <camila.dambrosio95@gmail.com>
2023-10-25 16:25:20 -0300

    added functionality to searchbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,30 +8,56 @@ import axios from 'axios'
 const Main = () => {
 
     let [eventos, setEventos] = useState([])
+    let [filteredEventos, setFilteredEventos] = useState([])
+    let [eventName, setEventName] = useState('');
 
     useEffect(() => {
         axios.get("https://mindhub-xj03.onrender.com/api/amazing")
-        .then((response) => {
-            setEventos(response.data.events)
-        })
-    }, [])
-
-
+          .then((response) => {
+            setEventos(response.data.events);
+          })
+      }, []);
+    
+      const filterEvents = (name) => {
+        const eventsFilter = eventos.filter(event => event.name.toLowerCase().includes(name.toLowerCase()));
+        setFilteredEventos(eventsFilter);
+      };
+    
+      const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        filterEvents(eventName);
+      };
+    
+      const handleInputChange = (e) => {
+        setEventName(e.target.value);
+        if (e.target.value === '') {
+          setFilteredEventos([]);
+        } else {
+          filterEvents(e.target.value);
+        }
+      };
+    
 
     return (
         <>
             <div className="search">
                 <Checkbox/>
-                <SearchFilter/>
+                <SearchFilter
+                    eventName={eventName}
+                    handleInputChange={handleInputChange}
+                    handleSearchSubmit={handleSearchSubmit}
+                />
             </div>
             <div className='event_cards'>
-            {
-                eventos.map((evento) => {
-                    return ( 
-                        // eslint-disable-next-line react/jsx-key
-                        <CardComponent evento={evento}/>
-                )})
-            }
+                {filteredEventos.length > 0 ? (
+                filteredEventos.map((evento, index) => (
+                    <CardComponent key={index} evento={evento} />
+                ))
+                ) : (
+                eventName ? <p>No se encontraron eventos que coincidan con su búsqueda.</p> : eventos.map((evento, index) => (
+                    <CardComponent key={index} evento={evento} />
+                ))
+                )}
             </div>
         </>
     )
