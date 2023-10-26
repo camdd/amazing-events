d19bc9cb5085ffbbfb46d7e9bc2fd9e05af20774
Cam <camila.dambrosio95@gmail.com>
2023-10-26 16:01:57 -0300

    i hate tables so much

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Nav() {
+    return(
+        <nav>
+            <Link to={"/contact"}> ir a contacto </Link>
+            <Link to={"/"}> ir a Home </Link>
+        </nav>
+    )
+}
+
+export default Nav
