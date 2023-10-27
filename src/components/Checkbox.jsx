a13be56ed9176f386843e6bd8893ac74a2734c14
Cam <camila.dambrosio95@gmail.com>
2023-10-27 14:58:07 -0300

    borré comments y cambié de lugar algunas cositas

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,28 +1,10 @@
 import './Checkbox.css'
-// Checkbox.jsx
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 
 // eslint-disable-next-line react/prop-types
-const Checkbox = ({ events, allEvents, container, dibujarEventos }) => {
-  // eslint-disable-next-line no-unused-vars
-  const [selectedCategories, setSelectedCategories] = useState([]);
+const Checkbox = () => {
 
-  useEffect(() => {
-    // eslint-disable-next-line no-unused-vars
-    const handleCheckboxChange = () => {
-      const selectedCategories = Array.from(document.querySelectorAll(".category-filter"))
-        .filter((checkbox) => checkbox.checked)
-        .map((checkbox) => checkbox.value);
-
-      if (selectedCategories.length === 0) {
-        dibujarEventos(allEvents, container);
-      } else {
-        // eslint-disable-next-line react/prop-types
-        const eventsFilter = events.filter((event) => selectedCategories.includes(event.category));
-        dibujarEventos(eventsFilter, container);
-      }
-    };
-  }, [events, allEvents, container, dibujarEventos]);
 
   return (
     <div className="barraCheck">
@@ -33,20 +15,20 @@ const Checkbox = ({ events, allEvents, container, dibujarEventos }) => {
         <label htmlFor="category2">
           <input type="checkbox" value="Books" id="category2" className="category-filter" /> Books
         </label>
-        <label htmlFor="category2">
-          <input type="checkbox" value="Museum" id="category2" className="category-filter" /> Museum
+        <label htmlFor="category3">
+          <input type="checkbox" value="Museum" id="category3" className="category-filter" /> Museum
         </label>
-        <label htmlFor="category2">
-          <input type="checkbox" value="Concert" id="category2" className="category-filter" /> Concert
+        <label htmlFor="category4">
+          <input type="checkbox" value="Concert" id="category4" className="category-filter" /> Concert
         </label>
-        <label htmlFor="category2">
-          <input type="checkbox" value="Race" id="category2" className="category-filter" /> Race
+        <label htmlFor="category5">
+          <input type="checkbox" value="Race" id="category5" className="category-filter" /> Race
         </label>
-        <label htmlFor="category2">
-          <input type="checkbox" value="Cinema" id="category2" className="category-filter" /> Cinema
+        <label htmlFor="category6">
+          <input type="checkbox" value="Cinema" id="category6" className="category-filter" /> Cinema
         </label>
-        <label htmlFor="category2">
-          <input type="checkbox" value="Party" id="category2" className="category-filter" /> Party
+        <label htmlFor="category7">
+          <input type="checkbox" value="Party" id="category7" className="category-filter" /> Party
         </label>
 
       </form>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,10 @@
+/* eslint-disable react/prop-types */
 import './Main.css'
 import CardComponent from './Card.jsx'
 import SearchFilter from './SearchFilter.jsx'
 import Checkbox from './Checkbox'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 
-
-//comparando con lo que hice en jsx vanilla, la diferencia en el código es que yo hice lo mismo en las tres vistas, usé mi código de main pero para pasados la condición era  if (event.assistance !== undefined) y para futuros if (event.estimate !== undefined). para esto hice tres archivos distintos y se repetía mucho código. acá mi desafío es que quiero haccer los tres en un mismo componente, qué podría hacer? en este componente yo incluí el checkbox y la searchbar porque se van a repetir en tres vistas y quiero evitar la repetición de código. tengo que hacer una función para past events y otra para upcoming, pero cómo puedo incluir TODO en el mismo return?
 
 const Main = (props) => {
    //siempre que cambia un estado de un componente se desmonta con los datos nuevos y se vuelve a montar
@@ -15,10 +13,6 @@ const Main = (props) => {
     let [eventName, setEventName] = useState('');
 
     useEffect(() => {
-        /* axios.get("https://mindhub-xj03.onrender.com/api/amazing")
-          .then((response) => {
-            setEventos(response.data.events);
-          }) */
           setEventos(props.events)
           setFilteredEventos(props.events) //el dato que me pasaron por propiedad lo voy a setear en el estado
 
@@ -46,7 +40,7 @@ const Main = (props) => {
 
     return (
         <>
-        <h1>{props.title}</h1>
+        <h1 className='page_title'>{props.title}</h1>
             <div className="search">
                 <Checkbox/>
                 <SearchFilter
